b6751a4a82ad24bc4e6fe750c52c231c81929526
mdimran1409036 <mdimran1409036@gmail.com>
2021-10-19 20:29:39 +0600

    image address changed

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -8,14 +8,14 @@ const Banner = () => {
             <Carousel.Item className="position-relative">
                 <img
                     className="d-block w-100"
-                    src="http://patterson.dr-patterson-vet.themerex.net/wp-content/uploads/2018/05/slider-vet-01.jpg"
+                    src="https://i.ibb.co/rwxXK2v/slider-vet-01.jpg"
                     alt="First slide"
                 />
                 <Carousel.Caption className="slider-details d-none d-lg-block">
                     <div className="p-3">
                         <h5 className="text-start">Animal Doctor</h5>
                         <p className="text-start">Professional care for your pets</p>
-                        <div className="w-25 mb-2" style={{ border: '1px solid #FF436B'}}></div>
+                        <div className="w-25 mb-2" style={{ border: '1px solid #FF436B' }}></div>
                         <p className="text-start">One of the essential qualities of a professional veterinarian is our patience. At our clinic, we  take a great care of you little furry friends.
                         </p>
                     </div>
@@ -24,7 +24,7 @@ const Banner = () => {
             <Carousel.Item className="position-relative">
                 <img
                     className="d-block w-100"
-                    src="http://patterson.dr-patterson-vet.themerex.net/wp-content/uploads/2018/05/slider-vet-02.jpg"
+                    src="https://i.ibb.co/471B0Sr/slider-vet-02.jpg"
                     alt="Second slide"
                 />
                 <Carousel.Caption className="slider-details d-none d-lg-block">
@@ -40,7 +40,7 @@ const Banner = () => {
             <Carousel.Item className="position-relative">
                 <img
                     className="d-block w-100"
-                    src="http://patterson.dr-patterson-vet.themerex.net/wp-content/uploads/2018/05/slider-vet-03.jpg"
+                    src="https://i.ibb.co/PtzyrkP/slider-vet-03.jpg"
                     alt="Third slide"
                 />
                 <Carousel.Caption className="slider-details d-none d-lg-block">
